5cf8da9cd2e051fae1832dde29e83a521e26f736
FellipeGodoi <115329585+FellipeGodoi@users.noreply.github.com>
2026-05-20 21:24:27 -0300

    fix: fixes payorder again

--- src/service/user/payOrder.ts
+++ src/service/user/payOrder.ts
@@ -4,11 +4,18 @@ export async function payOrder(
   orderId: string,
   payments: { cardId: string; amount: number }[]
 ) {
+  const token = localStorage.getItem("token")
+
   try {
     const response = await api.post(
       `/my-data/orders/${orderId}/pay`,
       {
         payments,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       }
     )
 
